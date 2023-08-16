34ab72f1f3c29951b93ed2e0851d34499977a517
fehenriq <lipe.h.r@gmail.com>
2023-08-16 10:09:46 -0300

    :sparkles: feat: Default router

--- setup/urls.py
+++ setup/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from rest_framework import permissions, routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 schema_view = get_schema_view(
@@ -34,7 +34,10 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
+router = routers.DefaultRouter()
+
 urlpatterns = [
+	path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token-obtain'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
