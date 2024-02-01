4ca4488832ac104ecd74b5f8fe37565dcb848f16
Felipe <lipe.h.r@gmail.com>
2024-02-01 10:01:59 -0300

    :sparkles: feat: return user id in token get

--- setup/urls.py
+++ setup/urls.py
@@ -3,15 +3,17 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions, routers
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from utils.token import CustomTokenObtainPairView
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="API",
+        title="Purchase Requisition API",
         default_version="v1",
         description="API Rest",
         terms_of_service="#",
-        contact=openapi.Contact(email="fehenriq.dev@gmail.com"),
+        contact=openapi.Contact(email="dev2@engenhadev.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
@@ -27,7 +29,7 @@ urlpatterns = [
     path("", include("apps.departments.urls")),
     path("", include("apps.products.urls")),
     path("", include("apps.requisitions.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token-obtain"),
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="token-obtain"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("api/swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("api/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
