45d1ef82542c496337c48baa491a3c71da48adc4
Felipe <lipe.h.r@gmail.com>
2024-02-09 14:51:27 -0300

    :bug: fix: adjust fields to gpb

--- apps/contracts/models.py
+++ apps/contracts/models.py
@@ -8,12 +8,10 @@ class Contract(models.Model):
     id = models.CharField(primary_key=True, unique=True, max_length=6)
     company = models.CharField(_("Company"), choices=COMPANIES, default="Gimi", max_length=4)
     contract_number = models.CharField(_("Contract Number"), max_length=10)
-    control_number = models.CharField(_("Control Number"), max_length=8)
+    control_number = models.CharField(_("Control Number"), max_length=8, null=True, blank=True)
     client_name = models.CharField(_("Client Name"), max_length=120)
     project_name = models.CharField(_("Client Name"), max_length=120, null=True, blank=True)
-    freight_value = models.DecimalField(
-        _("Freight Value"), max_digits=7, decimal_places=2, null=True, blank=True
-    )
+    freight_value = models.FloatField(_("Freight Value"), null=True, blank=True)
 
     def __str__(self):
         return self.contract_number
