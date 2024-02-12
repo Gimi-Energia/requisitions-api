53bc775d386664b588cff448f1b378e48f041e77
fehenriq <lipe.h.r@gmail.com>
2024-02-12 12:27:24 -0300

    :sparkles: feat: adjust for transporters endpoint

--- apps/providers/serializers.py
+++ apps/providers/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from apps.providers.models import Provider
+from apps.providers.models import Provider, Transporter
 
 
 class ProviderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Provider
+        fields = "__all__"
+
+
+class TransporterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transporter
         fields = "__all__"
--- apps/providers/urls.py
+++ apps/providers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from apps.providers.views import ProviderList, ProviderDetail
+from apps.providers.views import ProviderDetail, ProviderList, TransporterDetail, TransporterList
 
 urlpatterns = [
     path("api/providers/", ProviderList.as_view(), name="providers-list"),
     path("api/providers/<str:pk>/", ProviderDetail.as_view(), name="provider-detail"),
+    path("api/transporters/", TransporterList.as_view(), name="transporters-list"),
+    path("api/transporters/<str:pk>/", TransporterDetail.as_view(), name="transporter-detail"),
 ]
