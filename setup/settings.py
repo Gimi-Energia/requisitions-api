e25241f50d3fa8781f13ae338cf715cf28b921ab
Felipe <lipe.h.r@gmail.com>
2024-02-27 09:22:25 -0300

    :recycle: refactor: remove pagination

--- setup/settings.py
+++ setup/settings.py
@@ -150,8 +150,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 50,
+    # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    # "PAGE_SIZE": 50,
 }
 
 CORS_ORIGIN_ALLOW_ALL = False
