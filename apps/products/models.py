660dda07b3dd098f1a08a2f4a01e543414385c0d
Felipe <lipe.h.r@gmail.com>
2024-03-12 09:05:21 -0300

    :bug: fix: turns product code and contract number into unique

--- apps/products/models.py
+++ apps/products/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid4, editable=False)
-    code = models.CharField(_("Code"), max_length=30)
+    code = models.CharField(_("Code"), max_length=40, unique=True)
     un = models.CharField(_("Measurement Unit"), max_length=30)
     description = models.TextField(_("Description"))
 
