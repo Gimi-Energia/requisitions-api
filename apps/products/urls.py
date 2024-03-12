78690fcdd92d7a4def8867d61d42191e77a16ad6
Felipe <lipe.h.r@gmail.com>
2024-03-12 09:06:40 -0300

    :sparkles: feat: add endpoint to remove duplicated products

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from apps.products.views import ProductDetail, ProductList, ProductsDataAPIView
+from apps.products.views import (
+    ProductDetail,
+    ProductList,
+    ProductsDataAPIView,
+    remove_duplicates_view,
+)
 
 urlpatterns = [
     path("api/products/", ProductList.as_view(), name="products-list"),
+    path("api/products/remove_duplicates/", remove_duplicates_view, name="remove-duplicates"),
     path("api/products/iapp/", ProductsDataAPIView.as_view(), name="products-iapp"),
     path("api/products/<str:pk>/", ProductDetail.as_view(), name="product-detail"),
 ]
