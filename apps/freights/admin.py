c58033036ed32f8b22ce391f8775c43c75c21198
Felipe <lipe.h.r@gmail.com>
2024-03-21 08:25:18 -0300

    :sparkles: feat: add freights admin

--- apps/freights/admin.py
+++ apps/freights/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from apps.freights.models import Freight, FreightQuotation
 
-# Register your models here.
+
+class Freights(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "company",
+        "department",
+        "request_date",
+        "requester",
+        "status",
+        "approver",
+        "approval_date",
+    )
+    list_display_links = ("id",)
+    search_fields = (
+        "requester",
+        "approver",
+    )
+    list_filter = ("status",)
+    list_per_page = 25
+    ordering = ("request_date", "approval_date")
+
+
+class FreightQuotations(admin.ModelAdmin):
+    list_display = ("freight", "transporter", "name_other", "price", "status")
+    list_display_links = ("freight", "transporter")
+    search_fields = (
+        "freight",
+        "transporter",
+    )
+    list_filter = ("status",)
+    list_per_page = 25
+    ordering = ("freight", "transporter")
+
+
+admin.site.register(Freight, Freights)
+admin.site.register(FreightQuotation, FreightQuotations)
