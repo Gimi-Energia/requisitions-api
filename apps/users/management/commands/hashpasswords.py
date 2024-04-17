a2773716051e074cc876d5fdbd464ff329e6aa15
Felipe <lipe.h.r@gmail.com>
2024-04-17 20:19:00 -0300

    :bug: fix: hash password only for new users

--- apps/users/management/commands/hashpasswords.py
+++ apps/users/management/commands/hashpasswords.py
@@ -8,9 +8,10 @@ class Command(BaseCommand):
     help = "Hashes user passwords"  # python manage.py hashpasswords
 
     def handle(self, *args, **kwargs):
-        for user in User.objects.filter(type="Requester"):
-            user.password = make_password(user.password)
-            user.save()
-            self.stdout.write(
-                self.style.SUCCESS(f"Password for {user.email} was hashed successfully!")
-            )
+        for user in User.objects.all():
+            if len(user.password) < 32:
+                user.password = make_password(user.password)
+                user.save()
+                self.stdout.write(
+                    self.style.SUCCESS(f"Password for {user.email} was hashed successfully!")
+                )
