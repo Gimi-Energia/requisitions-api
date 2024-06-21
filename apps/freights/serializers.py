8be069f0eed5e9346a5f846331fdff2390caee36
Felipe <lipe.h.r@gmail.com>
2024-06-21 16:54:42 -0300

    :sparkles: feat: add internal freight exceptions

--- apps/freights/serializers.py
+++ apps/freights/serializers.py
@@ -24,36 +24,74 @@ class FreightSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if data.get("request_date") and not retroactive_date(data["request_date"]):
-            raise serializers.ValidationError(
-                {"request_date": "Não é permitido data retroativa."}
-            )
+            raise serializers.ValidationError({"request_date": "Não é permitido data retroativa."})
 
         return data
 
     def create(self, validated_data):
         quotations_data = validated_data.pop("freightquotation_set")
+        is_internal = validated_data.get("is_internal", False)
 
-        if len(set((quotation_data["transporter"],) for quotation_data in quotations_data)) != len(
-            quotations_data
-        ):
+        internal_transporter_count = sum(
+            1 for q in quotations_data if q["transporter"].name == "FRETE INTERNO GRUPO GIMI"
+        )
+
+        external_transporter_count = sum(
+            1 for q in quotations_data if q["transporter"].name != "FRETE INTERNO GRUPO GIMI"
+        )
+
+        if not is_internal and internal_transporter_count > 0:
             raise serializers.ValidationError(
-                {"quotations": "Não é permitido transportadoras repetidas."}
+                "Para 'FRETE INTERNO GRUPO GIMI', a requisição deve ser definida como INTERNA."
             )
 
-        freight = Freight.objects.create(**validated_data)
+        if is_internal:
+            if len(quotations_data) != 1:
+                raise serializers.ValidationError(
+                    "Para frete interno, deve haver apenas uma transportadora."
+                )
 
-        for quotation_data in quotations_data:
-            transporter = quotation_data["transporter"]
-            price = quotation_data["price"]
-            status = quotation_data["status"]
-            name_other = quotation_data.get("name_other")
-            FreightQuotation.objects.create(
-                freight=freight,
-                transporter=transporter,
-                price=price,
-                status=status,
-                name_other=name_other,
-            )
+            quotation_data = quotations_data[0]
+            transporter_name = quotation_data["transporter"].name
+            transporter_status = quotation_data["status"]
+            freight_status = validated_data["status"]
+
+            if transporter_name != "FRETE INTERNO GRUPO GIMI":
+                raise serializers.ValidationError(
+                    "Para frete interno, a transportadora deve ser 'FRETE INTERNO GRUPO GIMI'."
+                )
+
+            if transporter_status != "Approved" or freight_status != "Approved":
+                raise serializers.ValidationError(
+                    "Para frete interno, os status devem ser APROVADO."
+                )
+        else:
+            if internal_transporter_count > 0 and external_transporter_count > 0:
+                raise serializers.ValidationError(
+                    "Não é permitido incluir 'FRETE INTERNO GRUPO GIMI' junto com outras transportadoras."
+                )
+
+            if len(
+                set((quotation_data["transporter"],) for quotation_data in quotations_data)
+            ) != len(quotations_data):
+                raise serializers.ValidationError(
+                    {"quotations": "Não é permitido transportadoras repetidas."}
+                )
+
+            freight = Freight.objects.create(**validated_data)
+
+            for quotation_data in quotations_data:
+                transporter = quotation_data["transporter"]
+                price = quotation_data["price"]
+                status = quotation_data["status"]
+                name_other = quotation_data.get("name_other")
+                FreightQuotation.objects.create(
+                    freight=freight,
+                    transporter=transporter,
+                    price=price,
+                    status=status,
+                    name_other=name_other,
+                )
 
         return freight
 
