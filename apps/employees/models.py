cb7833cf89446d55b3e7137ff03ee01ef9d67429
augustocosta79 <amcneto@hotmail.com>
2024-11-21 13:47:34 -0300

    :sparkles: feat: Employee Position Table

--- apps/employees/models.py
+++ apps/employees/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from apps.departments.models import Department
 
 # Create your models here.
+class Position(models.Model):
+    id = models.CharField(max_length=7, primary_key=True, unique=True, editable=False)
+    name = models.CharField(_("Name"), max_length=50)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
--- apps/employees/urls.py
+++ apps/employees/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from views import EmployeeList
+from .views import EmployeeList
 
 urlpatterns = [
     path("api/employees/", EmployeeList.as_view(), name="employees-list"),
