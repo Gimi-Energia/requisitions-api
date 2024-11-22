a52557f02fced3b73395496aab3991ee7fca3698
augustocosta79 <amcneto@hotmail.com>
2024-11-22 16:05:26 -0300

    :wastebasket: remove: remove employee create route

--- apps/employees/urls.py
+++ apps/employees/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import EmployeeList, PositionList, EmployeeCreate, EmployeeDetail
+from .views import EmployeeList, PositionList, EmployeeDetail
 
 urlpatterns = [
     path("api/employees/", EmployeeList.as_view(), name="employees-list"),
     path("api/employees/position/", PositionList.as_view(), name="position-list"),
-    path("api/employees/create/", EmployeeCreate.as_view(), name="employee-create"),
     path("api/employees/<str:pk>/", EmployeeDetail.as_view(), name="employee-detail"),
 ]
