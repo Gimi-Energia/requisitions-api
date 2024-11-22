734a83e3ffa8f7e6666da97cb2339ea25ad85069
augustocosta79 <amcneto@hotmail.com>
2024-11-22 16:03:48 -0300

    :bug: fix: change EmployeeList to ListCreateApiView and remove CreateEmployee

--- apps/employees/views.py
+++ apps/employees/views.py
@@ -2,13 +2,11 @@ from rest_framework import generics
 
 from django_filters.rest_framework import DjangoFilterBackend
 
-from apps.departments.models import Department
-
 from .models import Position, Employee
 from .serializers import PositionsSerializer, EmployeeSerializer
 
 # Create your views here.
-class EmployeeList(generics.ListAPIView):
+class EmployeeList(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
@@ -19,10 +17,6 @@ class PositionList(generics.ListAPIView):
     filterset_fields = ["company", "cost_center__id"]
     # permission_classes = [IsAuthenticated]
     
-class EmployeeCreate(generics.CreateAPIView):
-    model = Employee
-    serializer_class = EmployeeSerializer
-
 class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
