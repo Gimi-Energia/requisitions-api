f530d8bba77ef17ed994c246f9be41ec8b071734
augustocosta79 <amcneto@hotmail.com>
2024-11-24 15:22:43 -0300

    :sparkles: feat: create postion detail route

--- apps/employees/urls.py
+++ apps/employees/urls.py
@@ -4,6 +4,7 @@ from .views import EmployeeList, PositionList, EmployeeDetail
 
 urlpatterns = [
     path("api/employees/", EmployeeList.as_view(), name="employees-list"),
-    path("api/employees/position/", PositionList.as_view(), name="position-list"),
     path("api/employees/<str:pk>/", EmployeeDetail.as_view(), name="employee-detail"),
+    path("api/employees/position/", PositionList.as_view(), name="position-list"),
+    path("api/employees/position/<str:pk>", PositionList.as_view(), name="position-detail"),
 ]
