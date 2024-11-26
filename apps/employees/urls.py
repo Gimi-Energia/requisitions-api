8475a024ecbd9261cc62be4df1574db558be333b
augustocosta79 <amcneto@hotmail.com>
2024-11-26 09:10:49 -0300

    :bug: fix: change route position to positions

--- apps/employees/urls.py
+++ apps/employees/urls.py
@@ -4,7 +4,7 @@ from .views import EmployeeList, PositionList, EmployeeDetail, PositionDetail
 
 urlpatterns = [
     path("api/employees/", EmployeeList.as_view(), name="employees-list"),
-    path("api/employees/position/", PositionList.as_view(), name="position-list"),
-    path("api/employees/position/<str:pk>", PositionDetail.as_view(), name="position-detail"),
+    path("api/employees/positions/", PositionList.as_view(), name="position-list"),
+    path("api/employees/positions/<str:pk>", PositionDetail.as_view(), name="position-detail"),
     path("api/employees/<str:pk>/", EmployeeDetail.as_view(), name="employee-detail"),
 ]
