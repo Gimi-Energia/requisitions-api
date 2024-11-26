ba29fe508196b00b81a7477b4a251665ed7a9160
augustocosta79 <amcneto@hotmail.com>
2024-11-26 18:17:58 -0300

    :sparkles: feat: add date validation to Employee creation

--- apps/employees/serializers.py
+++ apps/employees/serializers.py
@@ -5,6 +5,8 @@ from apps.employees.models import Position, Employee
 from apps.departments.serializers import DepartmentSerializer
 from apps.users.serializers import UserCustomSerializer
 
+from utils.validators.valid_date import retroactive_date
+
 
 class PositionWriteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -21,6 +23,19 @@ class EmployeeWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = "__all__"
+    
+    def validate(self, data):
+        if data.get("request_date") and not retroactive_date(data["request_date"]):
+            raise serializers.ValidationError({"request_date": "Não é permitido data retroativa."})
+
+        if data.get("created_at") and not retroactive_date(data["created_at"]):
+            raise serializers.ValidationError({"created_at": "Não é permitido data retroativa."})
+
+        if data.get("start_date") and not retroactive_date(data["start_date"]):
+            raise serializers.ValidationError({"start_date": "Não é permitido data retroativa."})
+
+        return data
+
 
 
 class EmployeeReadSerializer(serializers.ModelSerializer):
