b6f7d4ce96554d1bf8906b0e057b2e9954057fb1
augustocosta79 <amcneto@hotmail.com>
2024-11-27 11:10:08 -0300

    :bug: fix: add datetime checking to retroactive_date

--- utils/validators/valid_date.py
+++ utils/validators/valid_date.py
@@ -1,8 +1,12 @@
-from datetime import date
+from datetime import date, datetime
 
 
 def retroactive_date(input_date):
-    if input_date < date.today():
+    today = date.today()
+    if isinstance(input_date, datetime):
+        today = datetime.now()
+        
+    if input_date < today:
         return False
 
     return True
