b1fa4a0ef0fa6a17d273d005f149801523924e63
augustocosta79 <amcneto@hotmail.com>
2024-11-28 17:59:33 -0300

    :sparkles: feat: create email template for each status

---
+++ apps/employees/services/email_service_templates.py
@@ -0,0 +1,44 @@
+TEMPLATES = {
+    "RH": {
+        "Pending": """
+            <h3>RH</h3>
+            <ul>
+                <li>Abrir a vaga para o candidato selecionado.</li>
+                <li>Entrar em contato com o getor do setor</li>
+            </ul>
+            <br>
+        """,
+        "Opened": "<br>",
+        "Approved": "<br>",
+        "Denied": "<br>",
+        "Canceled": "<br>",
+    },
+
+    "TI": {
+        "Pending": """
+            <h3>TI</h3>
+            <ul>
+                <li>Verificar equipamentos</li>
+                <li>Verificar estoque ou reserva</li>
+                <li>Necessidade de compra</li>
+                <li>Softwares necessários</li>
+                <li>Lead time médio</li>
+            </ul>
+            <br>
+        """,
+
+        "Approved": """
+            <h3>TI</h3>
+            <ul>
+                <li>Criar e-mail</li>
+                <li>Criar login nos softwares</li>
+                <li>Criar login no ERP</li>
+            </ul>
+            <br>
+        """,
+
+        "Opened": "<br>",
+        "Denied": "<br>",
+        "Canceled": "<br>",
+    }
+}
