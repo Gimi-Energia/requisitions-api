158894fa61874e9dbc36774449b4d36fb0d7caf4
fehenriq <lipe.h.r@gmail.com>
2024-12-18 07:49:39 -0300

    :bug: fix: total price colspan

--- apps/purchases/services/email_service.py
+++ apps/purchases/services/email_service.py
@@ -45,7 +45,7 @@ def build_quotation_table(
 
     price_header = "<th>Preço Un.</th>" if include_price else ""
     total_row = (
-        f"<tr><td colspan='2'><b>Total</b></td><td></td><td><b>R$ {total_price:.2f}</b></td></tr>"
+        f"<tr><td colspan='3'><b>Total</b></td><td><b>R$ {total_price:.2f}</b></td></tr>"
         if include_price
         else ""
     )
