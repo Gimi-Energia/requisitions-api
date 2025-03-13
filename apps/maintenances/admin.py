efc28ac27606fb8cd627c71cbe6f6e295959c30f
augustocosta79 <amcneto@hotmail.com>
2025-03-13 19:03:17 -0300

    :bug: feat: add Maintenance model admin configuration with search and filter options

--- apps/maintenances/admin.py
+++ apps/maintenances/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.maintenances.models import Maintenance
+
+class Maintenances(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "requester",
+        "created_at",
+        "status",
+    )
+    list_display_links = ("id",)
+    search_fields = (
+        "name",
+        "status",
+        "requester",
+    )
+    list_filter = ("requester",)
+    list_per_page = 25
+    ordering = ("-created_at",)
+
+admin.site.register(Maintenance, Maintenances)
